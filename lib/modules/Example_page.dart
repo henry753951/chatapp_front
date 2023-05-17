25eb815cbfa5fea8a181385227614d191a81097d
binhan <qweqazhank123@gmail.com>
2023-05-18 00:22:49 +0800

    add profile

--- lib/modules/Example_page.dart
+++ lib/modules/Example_page.dart
@@ -18,23 +18,154 @@ class _ExamplePageState extends State<ExamplePage> {
       children: [
         Padding(
           padding: const EdgeInsets.only(left: 16, right: 16, top: 10),
-          child: Row(
-            children: [
-              Text("個人檔案",
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    foreground: Paint()
-                      ..shader = const LinearGradient(
+          child: Expanded(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Text("個人檔案",
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          foreground: Paint()
+                            ..shader = const LinearGradient(
+                              colors: [Colors.red, Colors.orange],
+                              begin: Alignment.centerLeft,
+                              end: Alignment.centerRight,
+                            ).createShader(Rect.fromLTWH(0, 0, 80, 70)),
+                        )),
+                  ],
+                ),
+                Stack(children: [
+                  CircleAvatar(
+                    radius: 50.0,
+                    backgroundColor: Color.fromARGB(255, 226, 235, 113),
+                  ),
+                  Positioned(
+                    right: 0,
+                    bottom: 0,
+                    child: Container(
+                      height: 30,
+                      width: 30,
+                      decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 67, 190, 72),
+                          shape: BoxShape.circle,
+                          border: Border.all(color: Colors.white, width: 3.0)),
+                    ),
+                  )
+                ]),
+                info(
+                  icon: Icon(CupertinoIcons.brightness, color: Colors.orange),
+                  title: "性命",
+                  value: "姓名",
+                ),
+                info(
+                  icon: Icon(Icons.check_circle),
+                  title: "沒有",
+                  value: "mail",
+                ),
+                info(
+                  icon: Icon(Icons.check_circle),
+                  title: "birthday",
+                  value: "birthday",
+                ),
+                info(
+                  icon: Icon(Icons.check_circle),
+                  title: "phone",
+                  value: "phone",
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                Container(
+                  height: 50,
+                  width: 350,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      gradient: LinearGradient(
                         colors: [Colors.red, Colors.orange],
                         begin: Alignment.centerLeft,
                         end: Alignment.centerRight,
-                      ).createShader(Rect.fromLTWH(0, 0, 80, 70)),
-                  )),
-            ],
+                      )),
+                  child: Center(
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "登出",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ],
     ));
+  }
+}
+
+class info extends StatelessWidget {
+  Icon icon;
+  String title;
+  String value;
+  info({
+    super.key,
+    required this.title,
+    required this.value,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.only(bottom: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 1,
+            ),
+            Container(
+                width: 350,
+                height: 40,
+                decoration: BoxDecoration(
+                    border: Border(
+                        bottom: BorderSide(
+                  color: const Color.fromARGB(255, 215, 215, 215),
+                  width: 1,
+                ))),
+                child: Row(children: [
+                  Row(
+                    children: [
+                      icon,
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        title,
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Expanded(child: SizedBox(), flex: 1),
+                  TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        value,
+                        style: TextStyle(
+                            fontSize: 16, height: 1.4, color: Colors.grey),
+                      )),
+                ]))
+          ],
+        ));
   }
 }
