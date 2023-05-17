046d23892da81584f19b724cac4007891574c2b5
henry <henryboy753951@gmail.com>
2023-05-17 20:50:58 +0800

    Hi

--- lib/components/chat.dart
+++ lib/components/chat.dart
@@ -1,13 +1,18 @@
 import 'package:chatapp/modules/chat_detail_page.dart';
 import 'package:flutter/material.dart';
 
-class ChatUsersList extends StatefulWidget{
+class ChatUsersList extends StatefulWidget {
   String text;
   String secondaryText;
   String image;
   String time;
   bool isMessageRead;
-  ChatUsersList({required this.text,required this.secondaryText,required this.image,required this.time,required this.isMessageRead});
+  ChatUsersList(
+      {required this.text,
+      required this.secondaryText,
+      required this.image,
+      required this.time,
+      required this.isMessageRead});
   @override
   _ChatUsersListState createState() => _ChatUsersListState();
 }
@@ -16,23 +21,43 @@ class _ChatUsersListState extends State<ChatUsersList> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: (){
-        Navigator.push(context, MaterialPageRoute(builder: (context){
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
           return ChatDetailPage();
         }));
       },
       child: Container(
-        padding: EdgeInsets.only(left: 16,right: 16,top: 10,bottom: 10),
+        padding: EdgeInsets.only(left: 16, right: 16, top: 10, bottom: 10),
         child: Row(
           children: <Widget>[
             Expanded(
               child: Row(
                 children: <Widget>[
-                  CircleAvatar(
-                    backgroundImage: AssetImage(widget.image),
-                    maxRadius: 30,
+                  Stack(
+                    children: [
+                      CircleAvatar(
+                        radius: 24.0,
+                        backgroundColor: Color.fromARGB(255, 226, 235, 113),
+                      ),
+                      Positioned(
+                        right: 0,
+                        bottom: 0,
+                        child: Container(
+                          height: 16,
+                          width: 16,
+                          decoration: BoxDecoration(
+                              color: Color.fromARGB(255, 67, 190, 72),
+                              shape: BoxShape.circle,
+                              border: Border.all(
+                                  color: Colors.white,
+                                  width: 3.0)),
+                        ),
+                      )
+                    ],
                   ),
-                  SizedBox(width: 16,),
+                  SizedBox(
+                    width: 16,
+                  ),
                   Expanded(
                     child: Container(
                       color: Colors.transparent,
@@ -40,8 +65,14 @@ class _ChatUsersListState extends State<ChatUsersList> {
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
                           Text(widget.text),
-                          SizedBox(height: 6,),
-                          Text(widget.secondaryText,style: TextStyle(fontSize: 14,color: Colors.grey.shade500),),
+                          SizedBox(
+                            height: 6,
+                          ),
+                          Text(
+                            widget.secondaryText,
+                            style: TextStyle(
+                                fontSize: 14, color: Colors.grey.shade500),
+                          ),
                         ],
                       ),
                     ),
@@ -49,7 +80,14 @@ class _ChatUsersListState extends State<ChatUsersList> {
                 ],
               ),
             ),
-            Text(widget.time,style: TextStyle(fontSize: 12,color: widget.isMessageRead?Colors.pink:Colors.grey.shade500),),
+            Text(
+              widget.time,
+              style: TextStyle(
+                  fontSize: 12,
+                  color: widget.isMessageRead
+                      ? Colors.pink
+                      : Colors.grey.shade500),
+            ),
           ],
         ),
       ),
