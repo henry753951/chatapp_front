9b07447c7ac3122f4bbb35ba08048dacde514d31
陳駿杰 <zxas040866@gmail.com>
2023-05-17 23:11:25 +0800

    invite

--- lib/components/chat_detail_page_appbar.dart
+++ lib/components/chat_detail_page_appbar.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ChatDetailPageAppBar extends StatelessWidget implements PreferredSizeWidget{
+class ChatDetailPageAppBar extends StatelessWidget
+    implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
@@ -14,29 +15,47 @@ class ChatDetailPageAppBar extends StatelessWidget implements PreferredSizeWidge
           child: Row(
             children: <Widget>[
               IconButton(
-                onPressed: (){
+                onPressed: () {
                   Navigator.pop(context);
                 },
-                icon: Icon(Icons.arrow_back,color: Colors.black,),
+                icon: Icon(
+                  Icons.arrow_back,
+                  color: Colors.black,
+                ),
               ),
-              SizedBox(width: 2,),
+              SizedBox(
+                width: 2,
+              ),
               CircleAvatar(
                 backgroundImage: AssetImage("images/userImage1.jpeg"),
                 maxRadius: 20,
               ),
-              SizedBox(width: 12,),
+              SizedBox(
+                width: 12,
+              ),
               Expanded(
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    Text("Jane Russel",style: TextStyle(fontWeight: FontWeight.w600),),
-                    SizedBox(height: 6,),
-                    Text("Online",style: TextStyle(color: Colors.green,fontSize: 12),),
+                    Text(
+                      "Jane Russel",
+                      style: TextStyle(fontWeight: FontWeight.w600),
+                    ),
+                    SizedBox(
+                      height: 6,
+                    ),
+                    Text(
+                      "Online",
+                      style: TextStyle(color: Colors.green, fontSize: 12),
+                    ),
                   ],
                 ),
               ),
-              Icon(Icons.more_vert,color: Colors.grey.shade700,),
+              Icon(
+                Icons.more_vert,
+                color: Colors.grey.shade700,
+              ),
             ],
           ),
         ),
--- lib/modules/chat_page.dart
+++ lib/modules/chat_page.dart
@@ -10,69 +10,130 @@ class ChatPage extends StatefulWidget {
 
 class _ChatPageState extends State<ChatPage> {
   void showModal() {
+    List<invite> Invite = [
+      invite(
+          text: "Jane Russel",
+          secondaryText: "Awesome Setup",
+          image: "images/userImage1.jpeg",
+          time: "Now",
+          id: "1"),
+      invite(
+          text: "Jane Russel",
+          secondaryText: "Awesome Setup",
+          image: "images/userImage1.jpeg",
+          time: "Now",
+          id: "2"),
+      invite(
+          text: "Jane Russel",
+          secondaryText: "Awesome Setup",
+          image: "images/userImage1.jpeg",
+          time: "Now",
+          id: "3"),
+      invite(
+          text: "Glady's Murphy",
+          secondaryText: "That's Great",
+          image: "images/userImage2.jpeg",
+          time: "Yesterday",
+          id: "4"),
+      invite(
+          text: "Jorge Henry",
+          secondaryText: "Hey where are you?",
+          image: "images/userImage3.jpeg",
+          time: "31 Mar",
+          id: "5"),
+    ];
     showModalBottomSheet(
         isScrollControlled: true,
         context: context,
         builder: (context) {
           return StatefulBuilder(
-            builder: (BuildContext context, StateSetter setState) {
-              return Container(
-                height: MediaQuery.of(context).size.height * 0.75,
-                color: Color(0xff737373),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                        topRight: Radius.circular(20),
-                        topLeft: Radius.circular(20)),
-                  ),
-                  child: Column(
-                    children: <Widget>[
-                      SizedBox(
-                        height: 16,
-                      ),
-                      Center(
-                        child: Container(
-                          height: 4,
-                          width: 50,
-                          color: Colors.grey.shade200,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      // ListView.builder(
-                      //   itemCount: menuItems.length,
-                      //   shrinkWrap: true,
-                      //   physics: NeverScrollableScrollPhysics(),
-                      //   itemBuilder: (context, index) {
-                      //     return Container(
-                      //       padding: EdgeInsets.only(top: 10, bottom: 10),
-                      //       child: ListTile(
-                      //         leading: Container(
-                      //           decoration: BoxDecoration(
-                      //             borderRadius: BorderRadius.circular(30),
-                      //             color: menuItems[index].color.shade50,
-                      //           ),
-                      //           height: 50,
-                      //           width: 50,
-                      //           child: Icon(
-                      //             menuItems[index].icons,
-                      //             size: 20,
-                      //             color: menuItems[index].color.shade400,
-                      //           ),
-                      //         ),
-                      //         title: Text(menuItems[index].text),
-                      //       ),
-                      //     );
-                      //   },
-                      // )
-                    ],
-                  ),
+              builder: (BuildContext context, StateSetter setState) {
+            return Container(
+              height: MediaQuery.of(context).size.height * 0.75,
+              color: Color(0xff737373),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(20),
+                      topLeft: Radius.circular(20)),
                 ),
-              );
-            }
-          );
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(
+                      height: 16,
+                    ),
+                    Center(
+                      child: Container(
+                        height: 4,
+                        width: 50,
+                        color: Colors.grey.shade200,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "好友邀請",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      child: ListView.builder(
+                        itemCount: Invite.length,
+                        shrinkWrap: true,
+                        physics: NeverScrollableScrollPhysics(),
+                        itemBuilder: (context, index) {
+                          return Container(
+                            padding: EdgeInsets.only(top: 10, bottom: 10),
+                            child: Dismissible(
+                              onDismissed: (direction) {
+                                // Remove the item from the data source.
+                                setState(() {
+                                  Invite.removeAt(index);
+                                });
+                              },
+                              background: Container(
+                                color: Colors.red,
+                                margin:
+                                    const EdgeInsets.symmetric(horizontal: 15),
+                                alignment: Alignment.centerRight,
+                                child: const Icon(
+                                  Icons.delete,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              key: UniqueKey(),
+                              child: ListTile(
+                                  leading: Container(
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(30),
+                                      ),
+                                      height: 50,
+                                      width: 50,
+                                      child: CircleAvatar(
+                                        backgroundImage:
+                                            AssetImage(Invite[index].image),
+                                      )),
+                                  title: Text(Invite[index].text),
+                                  subtitle: Text(Invite[index].secondaryText),
+                                  trailing: Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      Text(Invite[index].time),
+                                      Icon(Icons.check_circle),
+                                    ],
+                                  )),
+                            ),
+                          );
+                        },
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            );
+          });
         });
   }
 
